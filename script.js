8fe6f1d6b8ecbf646e0523da428f075cc628562f
David Jahns <imdavidjahns@yahoo.com>
2016-01-10 19:30:58 -0600

    threw some material on it

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function generateTicket(){
 
-
 	//lottery generator
 	var number; 
 	var ticket = [];
@@ -24,8 +23,11 @@ function generateTicket(){
 				number = generateNumber(); // generate a new number if number already used. 
 			}
 		}
-		ticket.push(" " + number); //add valid number to array
+		ticket.push("<li class='ball'> " + number + "</li>"); //add valid number to array
 	}
+
+	ticket = ticket.join(""); //turn array into string of numbers without comma
+
 
 	powerball = generatePowerball(); //generate powerball number 1-26
 
@@ -39,9 +41,38 @@ function generateTicket(){
 
 }
 
-document.getElementById("jackpot-button").onclick = function() {
 
+
+//READABLE MARKUP ON JACKPOT BUTTON ACTION BELOW
+//
+//<div class="loader">
+//   <svg class="circular" viewBox="25 25 50 50">
+//       <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+//   </svg>
+//</div>
+
+//swap function
+function swap() {
+	document.getElementById("loader").innerHTML = "";
+	document.getElementById("results").className = "";
+}
+
+//jackpot button action
+document.getElementById("jackpot-button").onclick = function() {
+	document.getElementById("results").className = "hidden";
+	document.getElementById("loader").innerHTML = "<div class='loader'><svg class='circular' viewBox='25 25 50 50'><circle class='path' cx='50' cy='50' r='20' fill='none' stroke-width='2' stroke-miterlimit='10'/></svg></div>";
 	generateTicket();
+	setTimeout(swap, 2000);
+};
+
+//show overlay on load. 
+window.onload = function() {
+	document.getElementById("overlay").className = "";
+};
+
+//overlay button cancel
+document.getElementById("go").onclick = function() {
+	document.getElementById("overlay").className = "overlay-scale";
 };
 
 
